1e54ee7ee1afe16b73b4a14afa1497bb1c63bdb5
samw0627 <hinhin0627@gmail.com>
2022-02-10 22:53:12 -0800

    Updated Student.java

--- Student.java
+++ Student.java
@@ -8,32 +8,65 @@ public class Student implements Comparable<Student> {
     private final String lastName;
     private final String PID;
 
-    public Student(String firstName, String lastName, String PID) {}
+    public Student(String firstName, String lastName, String PID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.PID = PID;
+    }
 
     public String getLastName() {
-        return null;
+        return this.lastName;
     }
 
     public String getFirstName() {
-        return null;
+        return this.firstName;
     }
 
     public String getPID() {
-        return null;
+        return this.PID;
     }
 
     @Override
     public boolean equals(Object o) {
-        return false;
+        if(o == null){
+            return false;
+        }
+        if(!this.firstName.equals((((Student) o).getFirstName()))){
+            return false;
+        }
+        if(!this.lastName.equals((((Student) o).getLastName())) ){
+            return false;
+        }
+        if(!this.getPID().equals((((Student) o).getPID()))){
+            return false;
+        }
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(firstName,lastName,PID);
     }
 
     @Override
     public int compareTo(Student o) {
-        return 0;
+        if(this.equals(o)){
+            return 0;
+        }
+        int lastCompare = this.lastName.compareTo(((Student)o).getLastName());
+        int firstCompare = this.firstName.compareTo((((Student) o).getFirstName()));
+        int PIDCompare = this.getPID().compareTo((((Student) o).getPID()));
+
+        if(lastCompare == 0){
+            if(firstCompare == 0){
+                return PIDCompare;
+            }
+            else{
+                return firstCompare;
+            }
+        }
+        else{
+            return lastCompare;
+        }
     }
 }
